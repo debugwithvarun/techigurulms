5e1062069172543899f6da2006cb72018996000f
Varunpython028 <varun_mit@techhubtechnology.com>
2026-02-19 22:19:26 +0530

    fix route

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,3 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
-
-// sdhjcbxm
-// dshjcnmx
-// bhkdjsvmx
